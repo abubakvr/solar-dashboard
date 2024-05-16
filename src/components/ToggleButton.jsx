f3797418e222a8550e329e3f6651536b032f74d4
sadeeqdev <boskitechh@gmail.com>
2024-05-16 06:33:51 +0100

    fix: chage header style

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -2,7 +2,7 @@ export const ToggleButton = ({ label, isActive, onClick, testId }) => (
   <button
     data-testid={testId}
     className={`text-sm font-bold w-max px-4 py-1 rounded focus:outline-none relative hover:bg-bright-blue hover:text-white ${
-      isActive && "bg-pale-blue text-indigo"
+      isActive && "bg-pale-blue text-indigo "
     }`}
     onClick={onClick}
   >
